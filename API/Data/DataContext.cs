0f4b0d2d5f20d89a45a7d200f4ffe34801b0972d
Matuska <pesicmatija@gmail.com>
2021-08-22 20:20:14 +0200

    Sve radi, sada krece nuget.org

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,7 +1,14 @@
+using API.Entitites;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
     }
 }
