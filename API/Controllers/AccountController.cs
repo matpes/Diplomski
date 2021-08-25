81b2350ca1f4eff867105fea3cd0f1d1d9ccced7
Matuska <pesicmatija@gmail.com>
2021-08-25 22:57:05 +0200

    Logovanje odradjeno za API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,6 +35,22 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if(user == null){
+                return Unauthorized("Inavlid username");
+            }
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for(int i = 0; i < computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]){
+                    return Unauthorized("Inavlid password");
+                }
+            }
+            return user;
+        }
+
 
         private async Task<bool> userExists(string username){
             return await _context.Users.AnyAsync(x => x.UserName == username);
